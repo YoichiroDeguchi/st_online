58d791e2b56c38ee06f2d57e132ecfae0bc953ea
YoichiroDeguchi <y.d.190801@gmail.com>
2023-04-08 09:57:57 +0900

    top css

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -9,9 +9,18 @@
     <title>ST_online</title>
 </head>
 <body>
-<div class="flex items-center flex-col justify-center h-screen">
-    <div class="text-center text-2xl font-bold mb-6">STオンライン</div>
-    <a class="px-6 py-3 bg-indigo-500 text-xl text-white rounded hover:bg-indigo-600" href="{{ route('login') }}">ログイン</a>
-</div>
+    <div class="min-h-screen flex flex-col">
+        <header class="p-6 bg-white shadow-md">
+            <div class="container mx-auto flex justify-end items-center">
+                <a href="{{ route('login') }}" class="bg-indigo-500 text-white px-4 py-2 rounded">ログイン</a>
+            </div>
+        </header>
+        <div class="flex-grow flex items-center justify-center">
+            <div class="text-center">
+                <h1 class="text-4xl font-thin text-gray-800 tracking-wider mb-4">STオンライン</h1>
+                <p class="text-lg font-semibold text-gray-600">Hanabi Co., Ltd.</p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
